a21e84260d04ee767643d7ad077c2981e649c157
AnuVk <anuatwork22@gmail.com>
2015-08-20 11:40:00 +0100

    launches camera but detection is not done

--- app/src/main/java/com/vijayarunkumar/anupama/barcodevisionapiexample/BarcodeScannerActivity.java
+++ app/src/main/java/com/vijayarunkumar/anupama/barcodevisionapiexample/BarcodeScannerActivity.java
@@ -3,23 +3,14 @@ package com.vijayarunkumar.anupama.barcodevisionapiexample;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Camera;
-import android.hardware.camera2.CameraAccessException;
-import android.hardware.camera2.CameraDevice;
-import android.hardware.camera2.CameraManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-import android.view.View;
-import android.widget.FrameLayout;
 
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.FocusingProcessor;
-import com.google.android.gms.vision.MultiProcessor;
 import com.google.android.gms.vision.Tracker;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
@@ -33,15 +24,11 @@ import butterknife.ButterKnife;
  * Created by Anu on 19/08/15.
  */
 public class BarcodeScannerActivity extends Activity implements SurfaceHolder.Callback{
-
     @Bind(R.id.surface_view)SurfaceView mSurfaceView;
 
     private CameraSource mCameraSource;
-    private Context mContext;
     private SurfaceHolder mSurfaceHolder;
     private BarcodeDetector mBarcodeDetector;
-    private Camera mCamera = null;
-    private Barcode mBarcode;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,31 +41,24 @@ public class BarcodeScannerActivity extends Activity implements SurfaceHolder.Ca
         mBarcodeDetector = new BarcodeDetector.Builder(this)
                 .setBarcodeFormats(Barcode.ITF | Barcode.PDF417| Barcode.QR_CODE)
                 .build();
-//        mBarcodeDetector.setProcessor(new FocusingProcessor<Barcode>(mBarcodeDetector, new Tracker<Barcode>()) {
-//
-//            @Override
-//            public int selectFocus(Detector.Detections<Barcode> detections) {
-//                SparseArray<Barcode> detectedItems = detections.getDetectedItems();
-//                for (int i = 0; i < detectedItems.size(); i++) {
-//                    Log.e(">>>", detectedItems.valueAt(i).rawValue);
-//                    int key = detections.getDetectedItems().keyAt(i);
-//                    return key;
-//                }
-//                return 0;
-//            }
-//        });
+        mBarcodeDetector.setProcessor(new BarcodeFocusingprocessor(mBarcodeDetector,new BarcodeTracker()));
+//                new MultiProcessor.Builder(new BarcodeTracker()).build());
 
-
-        mCameraSource = new CameraSource.Builder(this,mBarcodeDetector)
+        mCameraSource = new CameraSource.Builder(this, mBarcodeDetector)
                 .setFacing(CameraSource.CAMERA_FACING_BACK)
                 .setRequestedPreviewSize(1600, 1024)
-//                .setRequestedFps(15.0f)
                 .build();
 
         mSurfaceHolder = mSurfaceView.getHolder();
         mSurfaceHolder.addCallback(this);
-        mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
 
+    private void startCameraSource() {
+        try {
+            mCameraSource.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -86,73 +66,21 @@ public class BarcodeScannerActivity extends Activity implements SurfaceHolder.Ca
         try {
             mCameraSource.start(holder);
             Boolean isWorking = mBarcodeDetector.isOperational();
-            Log.d(">>>", "is barode detector working : " +isWorking);
-
-//            mBarcodeDetector.setProcessor(new BarcodeFocusingprocessor(mBarcodeDetector,new BarcodeTracker()));
-
-//            mSurfaceView.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    if (mCamera == null) {
-//                        mCamera = Camera.open();
-//                    }
-//                    mCamera.autoFocus(new Camera.AutoFocusCallback() {
-//                        @Override
-//                        public void onAutoFocus(boolean success, Camera camera) {
-//                            Log.d(">>>", "autofocus");
-//                        }
-//                    });
-//                }
-//            });
-//            mCamera = Camera.open();
-//            Camera.Parameters parameters = mCamera.getParameters();
-//            parameters.setAntibanding(Camera.Parameters.ANTIBANDING_AUTO);
-//            parameters.setVideoStabilization(true);
-//            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_MACRO);
-//            mCamera.setParameters(parameters);
-//
-//            mCamera.autoFocus(new Camera.AutoFocusCallback() {
-//                @Override
-//                public void onAutoFocus(boolean success, Camera camera) {
-//                    Log.d(">>>","autofocus is : " +success);
-//                }
-//            });
+            Log.d(">>>", "is barode detector working : " + isWorking);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void releaseCameraAndPreview() {
-        if (mCamera != null) {
-            mCamera.release();
-            mCamera = null;
-        }
-    }
-
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        mBarcodeDetector.setProcessor(new BarcodeFocusingprocessor(mBarcodeDetector,new BarcodeTracker()));
-
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        mCamera.setPreviewCallback(null);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mCamera.release();
-        mCameraSource.release();
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mCamera.release();
-        mCameraSource.release();
-    }
 
 private class BarcodeFocusingprocessor implements Detector.Processor<Barcode> {
     public BarcodeFocusingprocessor(BarcodeDetector barcodeDetector, BarcodeTracker barcodeTracker) {
@@ -172,7 +100,11 @@ private class BarcodeFocusingprocessor implements Detector.Processor<Barcode> {
     }
 }
 
-private class BarcodeTracker extends Tracker<Barcode> {
+private class BarcodeTracker extends Tracker<Barcode>{
+    @Override
+    public void onNewItem(int id, Barcode item) {
+        Log.d(">>>", "items are !!!!" + item);
+    }
 
     @Override
     public void onUpdate(Detector.Detections<Barcode> detections, Barcode item) {
@@ -180,4 +112,24 @@ private class BarcodeTracker extends Tracker<Barcode> {
 
     }
 }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mCameraSource.release();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCameraSource.release();
+    }
+
 }
